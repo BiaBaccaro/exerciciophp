c0461739778526c94d27ebf794427ff83ea9a832
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-09-28 12:00:45 -0300

    commit bia

--- exerc.php
+++ exerc.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
 
-    <h1>Operadores Lógicos</h1>
-
+    <h1 align= "center">Operadores Lógicos</h1>
+    
+    <div class="p">
     <?php
     
         $idade = ($_POST["idade"]);
@@ -25,11 +27,12 @@
 
         echo "$mensagem";
     ?>
-
+    
     <form method="post">
         <label for="idade">Digite a sua idade:</label>
         <input type="number" id="idade" name="idade" required>
         <button type="submit">Verificar</button>
     </form>
+    </div>
 </body>
 </html>
